88e189f95f0d01fc61845a0e80911315cefff49c
Ashish Gupta <ashishgup4949@gmail.com>
2025-02-05 19:47:21 +0530

    Update page.tsx

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -64,7 +64,7 @@ const columns = [
       const studentId = row.original.id;
       return (
         <div className="flex items-center gap-2">
-          <Link href={`/list/students/${studentId}`}>
+          <Link href={`/list/teachers/${studentId}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
               <Image src="/view.png" alt="" width={16} height={16} />
             </button>
